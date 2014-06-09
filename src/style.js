bbfad01124d5076fdc27e89dc79ffe13e21fda46
Nebukazar <justsoulay@gmail.com>
2014-06-09 22:28:29 +0200

    Change styles order

--- src/style.js
+++ src/style.js
@@ -1,9 +1,9 @@
-<%= grunt.file.read('src/css/Original.css') %>
 <%= grunt.file.read('src/css/Cleanup.css') %>
+<%= grunt.file.read('src/css/Original.css') %>
 <%= grunt.file.read('src/css/General.css') %>
 <%= grunt.file.read('src/css/Fonts.css') %>
-" + ($SS.conf["Sidebar Position"] !== 3 ? "<%= grunt.file.read('src/css/Sidebar.css') %>" : "") + "
 <%= grunt.file.read('src/css/Colors.css') %>
+" + ($SS.conf["Sidebar Position"] !== 3 ? "<%= grunt.file.read('src/css/Sidebar.css') %>" : "") + "
 <%= grunt.file.read('src/css/Icons.css') %>
 <%= grunt.file.read('src/css/Highlight.css') %>
 <%= grunt.file.read('src/css/ThreadWatcher.css') %>
